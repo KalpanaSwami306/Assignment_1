7ad0ab0de2be751742f1694fb7f299ce7caab917
KalpanaSwami306 <kalpanaswami603@gmail.com>
2024-09-20 15:20:27 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,27 @@
+public class Constant {
+    final String INPUT_STRING = "Which task do you want to execute?\n";
+    final String TASK1 = "1. Valid Parentheses Combination Generator";
+    final String TASK2 = "2. Digit Sum Loop(String)";
+    final String TASK3 = "3. Consecutive Number Summer ";
+    final String TASK4 = "4. Caesar Cipher with Shift Variability";
+    final String TASK5 = "5. Encoding challenge with ASCII Conversion";
+    final String EXIT = "6. Exit";
+    final String CHOICE = "Enter your choice from the above options: ";
+    final String ENTER_INPUT = "Enter the string:";
+    final String INPUT_NUMBER = "Enter the number(num >0 ):";
+    final String INVALID = "Invalid input";
+    final String SUM = "The sum of digits of ";
+    final String IS = " is ";
+    final String INVALID1 = "Invalid choice. Please try again.";
+    final String EXECUTE = "You want to execute the task if yes press 1 .For exit press any key:";
+    final String NO_SUM = "No Consecutive Sum";
+    final String INPUT_STRING1 = "Enter the input string: ";
+    final String SHIFT_NUMBER = "Enter the number of shifts in the pattern: ";
+    final String SHIFT_VALUE = "Enter the shift pattern values: ";
+    final String OUTPUT_STRING = "Encrypted string: ";
+    final String ARRAY_SIZE = "Enter the size of the input array(like 1,2,3)";
+    final String ARRAY_INPUT = "Enter the values of Array :";
+    final String SERIES_VALUES = " Enter the Series values : ";
+    final String INVALID_INDEX = "Invalid Argument";
+    final String INVALID_SERIES = "Invalid Series value";
+}
