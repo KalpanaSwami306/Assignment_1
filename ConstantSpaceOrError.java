9053c8ac7968f3ade0383ce51608ef660e3c7dc7
KalpanaSwami306 <kalpanaswami603@gmail.com>
2024-09-20 10:39:21 +0530

    Add files via upload

---
+++ ConstantSpaceOrError.java
@@ -0,0 +1,6 @@
+public class ConstantSpaceOrError {
+    final String SPACE = "   ";
+    final String INVALID_STRING = "Invalid Argument";
+    final String INVALID_RUN = "Invalid runs. Please enter a number between 0-6 except 5 ,7 for wide ball , 8 for No ball  or 'out' for wicket.\n";
+    final String INVALID_INPUT = "Invalid runs . Please enter between 0-6 except 5";
+}
